45bdfcbf619b3e01bbc156d8bd27d5b339d59d5a
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2020-08-31 20:00:02 -0600

    Mon Aug 31 2020

--- epi_judge_python/rook_attack.py
+++ epi_judge_python/rook_attack.py
@@ -1,12 +1,77 @@
 import copy
 from typing import List
+import itertools
 
 from test_framework import generic_test
 
+def rook_attack(A: List[List[int]]) -> None:
+    m, n = len(A), len(A[0])
+    
+    first_row_has_zero = 0 in A[0]
+    first_col_has_zero = any(not A[r][0] for r in range(n))
+
+    for row, col in itertools.product(range(1, m), range(1, n)):
+        if not A[row][col]:
+            A[row][0] = A[0][col] = 0
+
+    # Go over 1st col
+    for row in range(1, m):
+        # Set the entire row to 0
+        if A[row][0] == 0:
+            for col in range(n):
+                A[row][col] = 0
+    
+    # Go over 1st row
+    for col in range(1, n):
+        # Set the entire row to 0
+        if A[0][col] == 0:
+            for row in range(m):
+                A[row][col] = 0
+
+    # If first row has zero
+    if first_row_has_zero:
+        for col in range(col):
+            A[0][col] = 0
+    # If first col has zero
+    if first_col_has_zero:
+        for row in range(m):
+            A[row][0]= 0
+
 
 def rook_attack(A: List[List[int]]) -> None:
-    # TODO - you fill in here.
-    return
+    """In-place, using a flag"""
+    rows, cols = len(A), len(A[0])
+
+    for row, col in itertools.product(range(rows), range(cols)):
+        if A[row][col] == 0:
+            # Make the row and col in B equal to 0
+            for r in range(rows):
+                A[r][col] = "_" if A[r][col] != 0 else 0
+            for c in range(cols):
+                A[row][c] = "_" if A[row][c] != 0 else 0
+    
+    # Make all -1 into 0
+    for row, col in itertools.product(range(rows), range(cols)):
+        if A[row][col] == "_":
+            A[row][col] = 0
+
+
+def rook_attack_1(A: List[List[int]]) -> None:
+    """Using Additional Space"""
+    rows, cols = len(A), len(A[0])
+    B = copy.deepcopy(A)
+    
+    for row, col in itertools.product(range(rows), range(cols)):
+        if A[row][col] == 0:
+            # Make the row and col in B equal to 0
+            for r in range(rows):
+                B[r][col] = 0
+            for c in range(cols):
+                B[row][c] = 0
+    
+    # Copy B to A
+    for row, col in itertools.product(range(rows), range(cols)):
+        A[row][col] = B[row][col]
 
 
 def rook_attack_wrapper(A):
--- epi_judge_python_solutions/buy_and_sell_stock_k_times.py
+++ epi_judge_python_solutions/buy_and_sell_stock_k_times.py
@@ -4,18 +4,30 @@ from test_framework import generic_test
 
 
 def buy_and_sell_stock_k_times(prices: List[float], k: int) -> float:
-
+    # Edge cases
     if k == 0:
         return 0.0
+    # k >= n/2, optimum strategy is to by at valley and sell at peak
     elif 2 * k >= len(prices):
+        # Find the 
         return sum(max(0, b - a) for a, b in zip(prices[:-1], prices[1:]))
-    min_prices, max_profits = [float('inf')] * k, [0.0] * k
-    for price in prices:
-        for i in reversed(range(k)):
-            max_profits[i] = max(max_profits[i], price - min_prices[i])
-            min_prices[i] = min(min_prices[i],
-                                price - (0 if i == 0 else max_profits[i - 1]))
-    return max_profits[-1]
+
+    max_profit = [ [0] * len(prices) for _ in range(k + 1) ]
+
+    for num_transactions in range(1, k + 1):
+        for day in range(1, len(prices)):
+            # The maximum profit for given day and num_transactions is
+            # Not doing any transaction on the day
+            no_transaction = max_profit[num_transactions][day - 1]
+        
+            # Selling on that day and buying it at day m (m in 0, day - 1) 
+            # and the max_profit by doing one less transaction ending on day m
+            # NOTE: max_profit[num_transactions - 1][buy_day] is max profit for 1 less trasaction that ends on buy_day
+            selling_on_day = max( (prices[day] - prices[buy_day]) + max_profit[num_transactions - 1][buy_day] for buy_day in range(day) )
+
+            max_profit[num_transactions][day] = max(no_transaction, selling_on_day)
+
+    return max_profit[-1][-1]
 
 
 if __name__ == '__main__':
