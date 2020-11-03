56a71c34417178b3bfd098341ebcc42b8557fb4f
Sanchit Nevgi <sanchit.nevgi@gmail.com>
2020-11-02 20:08:42 -0700

    Mon Nov 02 2020

--- epi_judge_python/drawing_skyline.py
+++ epi_judge_python/drawing_skyline.py
@@ -9,8 +9,26 @@ Rect = collections.namedtuple('Rect', ('left', 'right', 'height'))
 
 
 def compute_skyline(buildings: List[Rect]) -> List[Rect]:
-    # TODO - you fill in here.
-    return []
+    min_left = min(building.left for building in buildings)
+    max_right = max(building.right for building in buildings)
+
+    heights = [0] * (max_right - min_left + 1)
+
+    # Create skyline
+    for building in buildings:
+        for b_idx in range(building.left, building.right + 1):
+            heights[b_idx - min_left] = max(heights[b_idx - min_left], building.height)
+        
+    # Merge skyline
+    left = 0
+    results = []
+    for i in range(1, len(heights)):
+        # Skyline changes, append the previous one
+        if heights[i] != heights[i - 1]:
+            results.append(Rect(min_left + left, i - 1 + min_left, heights[i - 1]))
+            left = i
+
+    return results + [Rect(left + min_left, max_right, heights[-1])]
 
 
 @enable_executor_hook
